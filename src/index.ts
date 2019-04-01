71c4ce814ef1f42b49075ce3c7d3a909b8c2341f
Hao Luo <howlowck@gmail.com>
2019-04-01 14:39:34 -0500

    enabled cors

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,12 @@ import transform from './routes/transform'
 
 let app = express()
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
 app.get('/search', search)
 app.get('/disambiguate', disambiguate)
 app.get('/transform', transform)
